41b0209cc68aa71f0d0b251243da6e03401bbb42
Ratovonirina <eric.ratovonirina@gmail.com>
2025-03-25 18:01:15 +0300

    remove ntf

--- src/app/modules/dashboard/dashboard-routing.module.ts
+++ src/app/modules/dashboard/dashboard-routing.module.ts
@@ -1,7 +1,6 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { DashboardComponent } from './dashboard.component';
-import { NftComponent } from './pages/nft/nft.component';
 import { PageHomeComponent } from './pages/example/page-home/page-home.component';
 import { PageHomeTwoComponent } from './pages/example/page-home-two/page-home-two.component';
 
@@ -13,7 +12,6 @@ const routes: Routes = [
       { path: '', redirectTo: 'home', pathMatch: 'full' },
       { path: 'home', component: PageHomeComponent },
       { path: 'home-two', component: PageHomeTwoComponent },
-      { path: 'ntfs', component: NftComponent },
       { path: '**', redirectTo: 'errors/404' },
     ],
   },
