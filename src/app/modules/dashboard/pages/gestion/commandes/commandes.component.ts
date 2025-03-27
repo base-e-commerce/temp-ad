711d8df6c04ccf3eede64cc64e6054debd263c83
Ratovonirina <eric.ratovonirina@gmail.com>
2025-03-27 20:00:02 +0300

    page produits

--- src/app/modules/dashboard/pages/gestion/commandes/commandes.component.ts
+++ src/app/modules/dashboard/pages/gestion/commandes/commandes.component.ts
@@ -1,12 +1,53 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { HeaderInfo } from 'src/app/core/interface/header-global';
+import { DivideComponent } from 'src/app/shared/components/divide/divide.component';
+import { GlobalHeaderComponent } from 'src/app/shared/components/global-header/global-header.component';
+import { TableContentComponent } from 'src/app/shared/components/table-content/table-content.component';
 
 @Component({
   selector: 'app-commandes',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, DivideComponent, GlobalHeaderComponent, TableContentComponent],
   templateUrl: './commandes.component.html',
-  styleUrl: './commandes.component.scss'
+  styleUrl: './commandes.component.scss',
 })
 export class CommandesComponent {
+  dataInfo: HeaderInfo = {
+    title: 'Commandes',
+    subtitle: 'Gestion',
+  };
 
+  tableHeaders = ['Name', 'Time Left', 'View', 'Title', 'View', 'Reel Image'];
+
+  tableData = [
+    {
+      title: 'Alice Dupont',
+      timeLeft: '3h 15m',
+      view: 'Voir Détails',
+      subtitle: 'Exploration en Montagne',
+      image: 'https://ratovonirina.onrender.com/assets/images/me4.jpg',
+    },
+    {
+      title: 'Bob Martin',
+      timeLeft: '2h 45m',
+      view: 'Voir Détails',
+      subtitle: 'Vie Urbaine Nocturne',
+      image: 'https://ratovonirina.onrender.com/assets/images/me4.jpg',
+    },
+    {
+      title: 'Charlie Bernard',
+      timeLeft: '4h 10m',
+      view: 'Voir Détails',
+      subtitle: 'Aventures Sous-Marines',
+      image: 'https://ratovonirina.onrender.com/assets/images/me4.jpg',
+    },
+    {
+      title: 'Diana Lefevre',
+      timeLeft: '1h 30m',
+      view: 'Voir Détails',
+      subtitle: 'Délices Culinaires',
+      image: 'https://ratovonirina.onrender.com/assets/images/me4.jpg',
+    },
+  ];
 }
--- src/app/modules/dashboard/pages/gestion/products/products.component.ts
+++ src/app/modules/dashboard/pages/gestion/products/products.component.ts
@@ -1,12 +1,65 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { HeaderInfo } from 'src/app/core/interface/header-global';
+import { DivideComponent } from 'src/app/shared/components/divide/divide.component';
+import { GlobalHeaderComponent } from 'src/app/shared/components/global-header/global-header.component';
+import { TableContentComponent } from 'src/app/shared/components/table-content/table-content.component';
 
 @Component({
   selector: 'app-products',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, GlobalHeaderComponent, DivideComponent, TableContentComponent],
   templateUrl: './products.component.html',
-  styleUrl: './products.component.scss'
+  styleUrl: './products.component.scss',
 })
 export class ProductsComponent {
+  dataInfo: HeaderInfo = {
+    title: 'Produits',
+    subtitle: 'Gestion',
+  };
 
+  tableHeaders = ['Name', 'Time Left', 'View', 'Title', 'View', 'Reel Image'];
+
+  tableData = [
+    {
+      image:
+        'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSdyvK9iusXBcBe9fx7rVS3joloIgyf4jDsH7fkjZZ7MvBdIr-lJpaaYbiF-KjvWS3VNcY&usqp=CAU',
+      title: 'T-shirt Noir',
+      subtitle: 'T-shirt unisexe en coton',
+      timeLeft: '2h 30m',
+      view: 'Voir les détails',
+    },
+    {
+      image:
+        'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSdyvK9iusXBcBe9fx7rVS3joloIgyf4jDsH7fkjZZ7MvBdIr-lJpaaYbiF-KjvWS3VNcY&usqp=CAU',
+      title: 'Robe Rouge',
+      subtitle: 'Robe élégante pour femme',
+      timeLeft: '1h 20m',
+      view: 'Voir les détails',
+    },
+    {
+      image:
+        'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSdyvK9iusXBcBe9fx7rVS3joloIgyf4jDsH7fkjZZ7MvBdIr-lJpaaYbiF-KjvWS3VNcY&usqp=CAU',
+      title: 'Portefeuille en Cuir',
+      subtitle: 'Portefeuille en cuir marron',
+      timeLeft: '3h 10m',
+      view: 'Voir les détails',
+    },
+    {
+      image:
+        'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSdyvK9iusXBcBe9fx7rVS3joloIgyf4jDsH7fkjZZ7MvBdIr-lJpaaYbiF-KjvWS3VNcY&usqp=CAU',
+      title: 'Baskets Noires',
+      subtitle: 'Baskets confortables pour homme',
+      timeLeft: '4h 5m',
+      view: 'Voir les détails',
+    },
+    {
+      image:
+        'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSdyvK9iusXBcBe9fx7rVS3joloIgyf4jDsH7fkjZZ7MvBdIr-lJpaaYbiF-KjvWS3VNcY&usqp=CAU',
+      title: 'Mug en Céramique',
+      subtitle: 'Mug blanc en céramique',
+      timeLeft: '1h 45m',
+      view: 'Voir les détails',
+    },
+  ];
 }
